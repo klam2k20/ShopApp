3ea5c5cacbe09c59f71b9528d6a8e36777df9ec8
klam2k20 <klam2k17@gmail.com>
2022-07-05 00:20:51 -0400

    Add template products onto Product Overview Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/products_overview_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,18 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('My Shop App')),
-      body: const Center(
-        child: Text('My Shop App'),
-      ),
+      home: ProductOverviewScreen(),
     );
   }
 }
